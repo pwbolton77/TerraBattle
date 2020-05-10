2c3bc8c55ff2574260b232a7377e0abeb45b84d7
pwbolton77 <pwbolton77@gmail.com>
2020-05-10 14:43:18 -0700

    Disable Friend related validation

--- TerraBattle.UI/Wrapper/Custom/UnitConfigWrapper.cs
+++ TerraBattle.UI/Wrapper/Custom/UnitConfigWrapper.cs
@@ -28,16 +28,16 @@ namespace TerraBattle.UI.Wrapper
                   new[] { nameof(OffenseRating) });
             }
 
-            if (string.IsNullOrWhiteSpace(FirstName))
-            {
-                yield return new ValidationResult("Firstname is required",
-                  new[] { nameof(FirstName) });
-            }
-            if (IsDeveloper && Emails.Count == 0)
-            {
-                yield return new ValidationResult("A developer must have an email-address",
-                  new[] { nameof(IsDeveloper), nameof(Emails) });
-            }
+            //if (string.IsNullOrWhiteSpace(FirstName))
+            //{
+            //    yield return new ValidationResult("Firstname is required",
+            //      new[] { nameof(FirstName) });
+            //}
+            //if (IsDeveloper && Emails.Count == 0)
+            //{
+            //    yield return new ValidationResult("A developer must have an email-address",
+            //      new[] { nameof(IsDeveloper), nameof(Emails) });
+            //}
         }
     }
 }
