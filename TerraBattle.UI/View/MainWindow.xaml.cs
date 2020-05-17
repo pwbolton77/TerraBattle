ccfb37dbafe55c742c2546e8fa974a746fde3b74
pwbolton77 <pwbolton77@gmail.com>
2020-05-17 06:57:45 -0700

    Have UnitConfig data entry done in a page (instead of the main window)

--- TerraBattle.UI/View/MainWindow.xaml.cs
+++ TerraBattle.UI/View/MainWindow.xaml.cs
@@ -1,24 +1,33 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
 using TerraBattle.UI.ViewModel;
 
 namespace TerraBattle.UI.View
 {
-  public partial class MainWindow : Window
-  {
-    private MainViewModel _viewModel;
-
-    public MainWindow(MainViewModel viewModel)
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
-      _viewModel = viewModel;
-      DataContext = _viewModel;
-    }
+        private MainViewModel _viewModel;
+        UnitConfigEditPage _unitConfigEditPage;
+        public MainWindow(MainViewModel viewModel)
+        {
+            InitializeComponent();
+            _viewModel = viewModel;
+            DataContext = _viewModel;   // @@ Dont think this is needed.
+            _unitConfigEditPage = new UnitConfigEditPage(_viewModel);   // @@ New could be done with AutoFac (?)
+            Loaded += MainWindowLoaded;
+        }
 
-    protected override void OnClosing(CancelEventArgs e)
-    {
-      base.OnClosing(e);
-      _viewModel.OnClosing(e);
+        private void MainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            MainWindowFrame.NavigationService.Navigate(_unitConfigEditPage);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            _viewModel.OnClosing(e);
+        }
     }
-  }
 }
---
+++ TerraBattle.UI/View/UnitConfigEditPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TerraBattle.UI.ViewModel;
+
+namespace TerraBattle.UI.View
+{
+    /// <summary>
+    /// Interaction logic for UnitConfigEditPage.xaml
+    /// </summary>
+    public partial class UnitConfigEditPage : Page
+    {
+        private MainViewModel _viewModel;
+
+        public UnitConfigEditPage(MainViewModel viewModel)
+        {
+            InitializeComponent();
+            _viewModel = viewModel;
+            DataContext = _viewModel;
+        }
+    }
+}
